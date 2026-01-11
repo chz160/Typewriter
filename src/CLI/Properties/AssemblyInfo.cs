904efb10980c561b311f3a99d602d0585c462c09
chz160 <noahporch@gmail.com>
2026-01-11 11:00:16 -0600

    rollback to beginning

--- src/CLI/Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("Typewriter.CLI")]
-[assembly: AssemblyDescription("Command-line interface for Typewriter - Generate TypeScript from C# templates")]
-[assembly: AssemblyConfiguration("")]
-
-[assembly: Guid("a1b2c3d4-e5f6-4a5b-9c8d-7e6f5a4b3c2d")]
